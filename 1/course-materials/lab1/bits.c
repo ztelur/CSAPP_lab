820a3dfd04466ddf5bb54ca944b2ff7d45b50693
ztelur <ztelur@gmail.com>
2015-06-03 22:15:36 +0800

    阅读lab1文档

--- 1/course-materials/lab1/bits.c
+++ 1/course-materials/lab1/bits.c
@@ -11,6 +11,7 @@
  * <stdio.h>, although you might get a compiler warning. In general,
  * it's not good practice to ignore compiler warnings, but in this
  * case it's OK.  
+ * 不要include stdio.h,但是还是可以使用printf
  */
 
 #if 0
@@ -23,12 +24,12 @@
 You will provide your solution to this homework by
 editing the collection of functions in this source file.
 
-INTEGER CODING RULES:
+INTEGER CODING RULES: //重点啊！！！！
  
   Replace the "return" statement in each function with one
   or more lines of C code that implements the function. Your code 
   must conform to the following style:
- 
+ //代码的格式必须是下边这种风格的啊。
   int Funct(arg1, arg2, ...) {
       /* brief description of how your implementation works */
       int var1 = Expr1;
@@ -41,33 +42,40 @@ INTEGER CODING RULES:
       return ExprR;
   }
 
-  Each "Expr" is an expression using ONLY the following:
+  Each "Expr" is an expression using ONLY the following://每个表达式都是下边
+  //的一种风格啊。
   1. Integer constants 0 through 255 (0xFF), inclusive. You are
       not allowed to use big constants such as 0xffffffff.
+	  0~255的整数
   2. Function arguments and local variables (no global variables).
+  	函数参数和局部变量，不能使用全局变量
   3. Unary integer operations ! ~
+  ！和～操作
   4. Binary integer operations & ^ | + << >>
-    
+    还有这些二元操作符
+
   Some of the problems restrict the set of allowed operators even further.
   Each "Expr" may consist of multiple operators. You are not restricted to
   one operator per line.
-
+	
+  //你禁止下边的操作啊。
   You are expressly forbidden to:
   1. Use any control constructs such as if, do, while, for, switch, etc.
-  2. Define or use any macros.
-  3. Define any additional functions in this file.
-  4. Call any functions.
-  5. Use any other operations, such as &&, ||, -, or ?:
-  6. Use any form of casting.
+  使用任何的控制判断语句
+  2. Define or use any macros.使用宏
+  3. Define any additional functions in this file.在这个文件中定义额外的函数
+  4. Call any functions.调用其他函数
+  5. Use any other operations, such as &&, ||, -, or ?:使用其他的操作符
+  6. Use any form of casting.使用类型转换
   7. Use any data type other than int.  This implies that you
-     cannot use arrays, structs, or unions.
+     cannot use arrays, structs, or unions.不能使用int外的数据类型，array之类的啊。
 
  
   You may assume that your machine:
   1. Uses 2s complement, 32-bit representations of integers.
-  2. Performs right shifts arithmetically.
+  2. Performs right shifts arithmetically.//算数右移
   3. Has unpredictable behavior when shifting an integer by more
-     than the word size.
+     than the word size. //移位大于位数的不可估计的结果
 
 EXAMPLES OF ACCEPTABLE CODING STYLE:
   /*
@@ -95,12 +103,14 @@ NOTES:
      that you are allowed to use for your implementation of the function. 
      The max operator count is checked by dlc. Note that '=' is not 
      counted; you may use as many of these as you want without penalty.
+	 //注意每个函数都有使用操作符的最大的数量
   3. Use the btest test harness to check your functions for correctness.
+  	
   4. The maximum number of ops for each function is given in the
      header comment for each function. If there are any inconsistencies 
      between the maximum ops in the writeup and in this file, consider
      this file the authoritative source.
-
+	//在每个函数之前啊。
 /*
  * STEP 2: Modify the following functions according the coding rules.
  * 
